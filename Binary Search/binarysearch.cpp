24dad568ef25b1c61c2bc1639e11710d08c1e8eb
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-02-07 18:28:05 +0600

    Binary Search, Lower Bound and Upper Bound Implemented and matched with STL

--- Binary Search/binarysearch.cpp
+++ Binary Search/binarysearch.cpp
@@ -1,7 +1,12 @@
 /*
-Problem Link  :
+Problem Link  : Binary Search, Lower Bound and Upper Bound Implemented and matched with STL
 About Problem : Binary Search Implementation
 Tags          : 
+Notes         :
+        lower_bound returns an iterator pointing to the first element 
+        in the range [first,last) which has a value not less than ‘val’. 
+        upper_bound returns an iterator pointing to the first element in the 
+        range [first,last) which has a value greater than ‘val’. 
  */
 
 #include<bits/stdc++.h>
@@ -11,26 +16,56 @@ void printarray(int arr[],int low, int high){
     for(int i=low; i<=high; i++) printf("%d ",arr[i]);
     printf("\n");
 }
-int BinarySearch(int arr[], int N, int value){
+int BSUpperBound(int arr[], int N, int value) {
     int low = 0,high = N-1, mid;
-
-    while(low<=high){
-        printarray(arr,low,high);
+    while (low < high) {
+        //printarray(arr,low,high);
+        mid = low+ (high-low)/2;
+        if(arr[mid]>value)  high = mid;
+        else low= mid+1;
+    }
+    if(value>=arr[N-1]) low++; //To match built in functions
+    return low;
+}
+int BSLowerBound(int arr[], int N, int value) {
+     int low = 0,high = N-1, mid;
+    while (low < high) {
+        //printarray(arr,low,high);
+        mid = low+ (high-low)/2;
+        if(arr[mid]>=value) {
+            high = mid;
+        } 
+        else {
+            low= mid+1;
+        }
+    }
+    if(value>=arr[N-1]) low++; //To match built in functions
+    return low;
+}
+int BinarySearch(int arr[], int N, int value){
+   
+    int low = 0,high = N-1, mid;
+    while(low<=high){ //Equal Sign is a must or else false result!
+        //printarray(arr,low,high);
         mid = (high+low)/2;
         if(arr[mid]>value) high=mid-1;
         else if( arr[mid]<value) low = mid+1;
-        else return mid;
-        
+        else return 1;
     }
-    return low;//lower Bound returning
+    return -1;
 
 }
 void Solution(){
-    int arr[] = {2, 5, 23, 45, 78, 345, 768, 987, 1098, 1099};
-    int N= 10;
+    int arr[] = {2, 5,5,5,5,5 ,23, 45, 78, 345, 768, 987, 1098, 1099};
+    int N= 14;
     
-    cout<<arr[BinarySearch(arr,N,7)];
-	
+    //cout<<BSLowerBound(arr,N,3);
+    for(int i=0; i<N; i++){
+        cout<<lower_bound(arr,arr+N,arr[i]+1)-arr<<" Lower Bound "<<BSLowerBound(arr,N,arr[i]+1)<<endl;
+
+        cout<<upper_bound(arr,arr+N,arr[i]+1)-arr<<" Upper Bound "<<BSUpperBound(arr,N,arr[i]+1)<<endl;
+    }
+    	
 }
 
 int main(){
