2483e85958a293e2e04f050ae02ed1dcde56ed78
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-02-10 21:41:18 +0600

    Working on Binary Search Lecture of Jami Sir in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 /*
-Problem Link  :
+Problem Link  : https://onlinejudge.org/external/120/12032.pdf
 About Problem :
 Tags          : 
  */
@@ -7,8 +7,26 @@ Tags          :
 #include<bits/stdc++.h>
 using namespace std;
 const int MAXN = 1e5;
-
+int Ans
 void Solution(){
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++)
+	{
+		int t, arr[MAXN], mindiff=0, diff;
+		cin>>t;
+		for(int i=0; i<t; i++) 
+		{
+			cin>>arr[i];
+			if(i==0) mindiff=arr[0];
+			else {
+				diff = arr[i]-arr[i-1];
+				if(diff>mindiff) mindiff=diff; 
+			}
+		}
+		
+		printf("Case %d: %d\n", i+1,mindiff);
+	}
 	
 	
 }
