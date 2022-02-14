02d5e3d0a0a26094d9981b5e167bc25d0bf85d09
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-02-15 00:42:33 +0600

    Binary Tree Deep dive

--- Binary Tree/BinaryTree.cpp
+++ Binary Tree/BinaryTree.cpp
@@ -1,9 +1,3 @@
-/*
-Problem Link  :
-About Problem : Binary Tree Implementation
-Tags          :
- */
-
 #include <bits/stdc++.h>
 using namespace std;
 const int MAXN = 1e5;
@@ -31,113 +25,43 @@ class BinaryTree
 {
 private:
     Node *binary_tree;
-
 public:
-    BinaryTree()
+            BinaryTree          ()
     {
         this->binary_tree = NULL;
     }
-    BinaryTree(Node *temp)
+            BinaryTree          (Node *binary_tree)
     {
-        this->binary_tree = temp;
+        this->binary_tree = binary_tree;
     }
-    void insertfunction(Node **binary_tree, int value)
-    {
-        Node *tmp = NULL;
-        if (!(*binary_tree))
-        {
-            tmp = new Node(value);
-            *binary_tree = tmp;
-            return;
-        }
-        if (value < (*binary_tree)->data)
-        {
-            insertfunction(&(*binary_tree)->left_child, value);
-        }
-        else if (value > (*binary_tree)->data)
-        {
-            insertfunction(&(*binary_tree)->right_child, value);
-        }
-    }
-    Node *searchfunction1(Node **binary_tree, int value)
-    {
-        if (!(*binary_tree))
-        {
-            return NULL;
-        }
-        if (value == (*binary_tree)->data)
-        {
-            return *binary_tree;
-        }
-        else if (value < (*binary_tree)->data)
-        {
-            searchfunction1(&((*binary_tree)->left_child), value);
-        }
-        else if (value > (*binary_tree)->data)
-        {
-            searchfunction1(&((*binary_tree)->right_child), value);
-        }
-        return NULL;
-    }
-    void insert(int value)
-    {
-        insertfunction(&this->binary_tree, value);
-    }
-    Node *search(Node *binary_tree, int value)
+    void    print_preorder      (Node *binary_tree)
     {
         if (binary_tree)
         {
-            Node *temp1 = search(binary_tree->left_child, value);
-            if(temp1!=NULL)return temp1;
-            if (binary_tree->data == value)
-            if(binary_tree!=NULL) return binary_tree;
-            Node *temp2 = search(binary_tree->right_child, value);
-            if(temp2!=NULL) return temp2;
-        }
-        return NULL;
-    }
-    void print_preorder(Node *binary_tree)
-    {
-        if (binary_tree)
-        {
-            printf("%d\n", binary_tree->data);
+            printf("%d ", binary_tree->data);
             print_preorder(binary_tree->left_child);
             print_preorder(binary_tree->right_child);
         }
     }
-    void print_inorder(Node *binary_tree)
+    void    print_inorder       (Node *binary_tree)
     {
         if (binary_tree)
         {
             print_inorder(binary_tree->left_child);
-            printf("%d\n", binary_tree->data);
+            printf("%d ", binary_tree->data);
             print_inorder(binary_tree->right_child);
         }
     }
-    void print_postorder(Node *binary_tree)
+    void    print_postorder     (Node *binary_tree)
     {
         if (binary_tree)
         {
             print_postorder(binary_tree->left_child);
             print_postorder(binary_tree->right_child);
-            printf("%d\n", binary_tree->data);
+            printf("%d ", binary_tree->data);
         }
     }
-    int height1(Node *node)
-    {
-        if (node == NULL)
-            return -1;
-        else
-        {
-            int left_depth = height(node->left_child);
-            int right_depth = height(node->right_child);
-            if (left_depth > right_depth)
-                return (left_depth + 2); // interms of madepth return  left_depth+1)
-            else
-                return (right_depth + 2);
-        }
-    }
-    int height(Node *binary_tree)
+    int     height              (Node *binary_tree)
     {
         int depth = 0;
 
@@ -170,7 +94,7 @@ public:
         }
         return depth;
     }
-    void print_levelorder(Node *binary_tree)
+    void    print_levelorder    (Node *binary_tree)
     {
         /* Function to line by line print level order traversal a tree*/
         int h = height(binary_tree);
@@ -178,10 +102,10 @@ public:
         for (i = 1; i <= h; i++)
         {
             printGivenLevel(binary_tree, i);
-            printf("\n");
+            // printf("\n");
         }
     }
-    void printGivenLevel(Node *binary_tree, int level)
+    void    printGivenLevel     (Node *binary_tree, int level)
     {
         /* Print nodes at a given level */
         if (binary_tree == NULL)
@@ -194,94 +118,50 @@ public:
             printGivenLevel(binary_tree->right_child, level - 1);
         }
     }
-    Node *Returntreehead()
+    int     numberofnodes       (Node *binary_tree){
+        if (binary_tree)
+        {
+            return numberofnodes(binary_tree->left_child)+numberofnodes(binary_tree->right_child)+1;
+        }
+        else return 0;
+
+    }
+    Node*   BuildTreePreIn      (int in[], int pre[], int inStrt, int inEnd)
+    {
+        static int preIndex = 0;
+        if (inStrt > inEnd) return NULL;
+        Node *tNode = new Node(pre[preIndex++]);
+        if (inStrt == inEnd) return tNode;
+        int inIndex = -1;
+        for (int i = inStrt; i <= inEnd; i++) if (in[i] == tNode->data) inIndex = i;
+        tNode->left_child = BuildTreePreIn(in, pre, inStrt, inIndex - 1);
+        tNode->right_child = BuildTreePreIn(in, pre, inIndex + 1, inEnd);
+        return tNode;
+    }
+    void    BuildTree           (int in[], int pre[], int inStrt, int inEnd)
+    {
+        this->binary_tree = BuildTreePreIn(in,pre, inStrt, inEnd);
+
+    }
+    Node*   Returntreehead      ()
     {
         return binary_tree;
     }
 };
-int search(int arr[], int strt, int end, int value)
-{
-	int i;
-	for (i = strt; i <= end; i++)
-	{
-		if (arr[i] == value)
-			return i;
-	}
-    return -1;
-}
-Node* newNode(int data)
-{
-    Node* Nnode = new Node();
-    Nnode->data = data;
-    Nnode->left_child = NULL;
-    Nnode->right_child = NULL;
- 
-    return (Nnode);
-}
-Node* buildTree(int in[], int pre[], int inStrt, int inEnd)
-{
-	static int preIndex = 0;
-
-	if (inStrt > inEnd)
-		return NULL;
-	Node* tNode = newNode(pre[preIndex++]);
-
-	if (inStrt == inEnd)
-		return tNode;
-	int inIndex = search(in, inStrt, inEnd, tNode->data);
-	tNode->left_child = buildTree(in, pre, inStrt, inIndex - 1);
-	tNode->right_child = buildTree(in, pre, inIndex + 1, inEnd);
-
-	return tNode;
-}
 
 int main()
 {
-    int n, in[MAXN],pre[MAXN];
-    cin>>n;
-    for(int i=0; i<n; i++) scanf("%d",&pre[i]);
-    for(int j=0; j<n; j++) scanf("%d",&in[j]);
-	int len = n;
-	Node* root = buildTree(in, pre, 0, len - 1);
-    BinaryTree t1 = BinaryTree(root);
+    int n, in[MAXN], pre[MAXN];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        scanf("%d", &pre[i]);
+    for (int j = 0; j < n; j++)
+        scanf("%d", &in[j]);
+    BinaryTree t1;
+    t1.BuildTree(in, pre, 0, n - 1);
     t1.print_postorder(t1.Returntreehead());
     printf("\n");
     t1.print_levelorder(t1.Returntreehead());
     printf("\n");
+    cout<<t1.numberofnodes(t1.Returntreehead())<<endl;
 }
-
-// void Solution()
-// {
-//     // BinaryTree t1 = BinaryTree();
-//     // t1.insert(10);
-//     // t1.insert(27);
-//     // t1.insert(39);
-//     // t1.insert(11);
-//     // t1.insert(8);
-//     // t1.insert(4);
-//     // t1.insert(6);
-//     // t1.insert(3);
-//     // t1.insert(2);
-//     // postorder
-//     // preorder
-//     // levelorder
-//     int arr[] = {2, 3, 4, 6, 8, 10, 11, 27, 39};
-//     // vector<int> preorder= {10,8,4,3,2,6,27,11,39} ;//preorder
-
-//     // vector<int> inorder = preordertoinorder(preorder);
-//     BinaryTree t2 = BinaryTree(build_tree_inorder(arr, 0, 8));
-//     Node * temp = t2.search(t2.Returntreehead(), 3);
-//     if(temp==NULL) cout<<"Data Not Found"<<endl;
-//     else printf("Data Found! %d",temp->data );
-
-// }
-
-// int main()
-// {
-//     // ios_base::sync_with_stdio(false);
-//     // cin.tie(NULL);
-
-//     Solution();
-
-//     return 0;
-// }
