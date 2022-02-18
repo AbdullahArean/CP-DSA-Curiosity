5eb5e756306a6aa866f1194506990fbc2e2cd4da
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-02-18 09:22:43 +0600

    Template test.cpp updated

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 /*
-Problem Link  : https://onlinejudge.org/external/120/12032.pdf
+Problem Link  : 
 About Problem :
 Tags          : 
  */
@@ -10,21 +10,9 @@ const int MAXN = 1e5;
 void Solution(){
 	int n;
 	cin>>n;
-	for(int i=0; i<n; i++)
-	{
-		int t, arr[MAXN], mindiff=0, diff;
-		cin>>t;
-		for(int i=0; i<t; i++) 
-		{
-			cin>>arr[i];
-			if(i==0) mindiff=arr[0];
-			else {
-				diff = arr[i]-arr[i-1];
-				if(diff>mindiff) mindiff=diff; 
-			}
-		}
+	for(int i=0 ; i<n; i++){
 		
-		printf("Case %d: %d\n", i+1,mindiff);
+		printf("Case %d:\n", i+1);
 	}
 	
 	
