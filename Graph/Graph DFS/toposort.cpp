d7f5d8f8e9aa0db00bb9ccbb87bb8cc996e5803a
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2022-08-02 12:36:31 +0600

    topoworl+k

--- Graph/Graph DFS/toposort.cpp
+++ Graph/Graph DFS/toposort.cpp
@@ -1,143 +1,186 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long int
 #define vi vector<int>
 #define pii pair<int, int>
 #define vii vector<pii>
-#define rep(i,a,b) for(int i=a; i<b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
 #define ff first
 #define ss second
 #define pqmaxi(a) priority_queue<int, vi> a
 #define pqmini(a) priority_queue<int, vi, greater<int>> a
 
-const ll MAXN=1e5;
-class Graph{
-    int n; 
+const ll MAXN = 1e5;
+class Graph
+{
+    int n;
     vector<int> adjlist[MAXN];
 
     int time;
-    int d[MAXN], f[MAXN], v[MAXN];
+    int d[MAXN], f[MAXN], v[MAXN], p[MAXN], low[MAXN];
 
     bool cycle;
-    vector<int> topo;
+    stack<int> topostack;
 
-    public:
-    Graph(int n){
-        this->n=n;
-    }
-    void add(int s , int d){
-        adjlist[s].push_back(d);
-    }
-    void dfs(){
-        for(int i=0; i<n; i++){
-            v[i]=0;
-            d[i]=-1;
-            f[i]=-1;
-        }
-        cycle=false;
-        time=0;
-        for(int j=0; j<n; j++){
-            if(v[j]==0) dfs_visit(j);
-        }
-        cout<<cycle<<endl;
-        for(auto k:topo){
-            cout<<k+1<<" ";
-        }
-        cout<<"\n";
-    }
-void dfs_visit(int u){
-    time= time+1;
-    d[u]=time;
-    v[u]=1;
-    //sort(adjlist[u].begin(), adjlist[u].end(), greater <>());
-    for(auto ad :adjlist[u]){
-        if(v[ad]==0){
-            dfs_visit(ad);
-        }
-        else if(v[ad]==0){
-            //
-            cout<<u<<"   "<<ad<<endl;
-            cycle=true;
+    vector<vector<int>> allpaths;
+    vector<int> path;
+    bool pathexists = false;
 
-        }
-            
-        }
-    
-    time=time+1;
-    f[u]=time;
-    v[u]=2;
-    topo.push_back(u);
-}
-
-};
-class graph{
-    int n;
-    vector<int> adjlist[MAXN];
-    ll time;
-    ll d[MAXN];
-    ll f[MAXN];
-    ll v[MAXN];
-    vector<int> topo;
-    bool cycle=false;
-
-
-    public: 
-    graph(int n){
-        this->n= n;
-    }
-    void add_new_edge(int s, int d){
-         adjlist[s].push_back(d);
-    }
-    void specialtoposort(){
-        cycle= false;
-        for(int i=0; i<n; i++){
-            v[i]=0;
-            f[i]= INT_MIN;
-            d[i]=INT_MIN;
-            //For Topological Sort
-            //while(!topo.empty()) topo.pop();
-        }
-        time=0;
-        for(int i=0; i<n; i++){
-            if(v[i]==0){
-                dfs_visit(i);
-            }
-        }
-        if(!cycle){
-            for(int p=topo.size()-1; p>=0; p++) cout<<topo[p]<<" ";
-            cout<<"\n";
-        }
-        else{
-            printf("Sandro fails.\n");
-        }
-
-    }
-    void dfs_visit(int i){
-        time++;
-        d[i]=time;
-        v[i]=1;
-        sort(adjlist[i].begin(),adjlist[i].end());
-        for (int j = adjlist[i].size() - 1; j >= 0; j--){
-                if(v[adjlist[i][j]]==0) dfs_visit(adjlist[i][j]);
-            }
-        v[i]=2;
-        time++;
-        f[i]= time;
-        //For Topological Sort
-        topo.push_back(i);
-        //cout<<i+1<<endl;
-    }
-    void printadjlist()
+public:
+    Graph(int n)
     {
-        for (int i = 0; i <n; i++)
-        {
-            cout << i+1 << " : ";
-        for(auto it=adjlist[i].begin(); it!= adjlist[i].end(); it++){
-         cout << *it+1<< " ";
-        }
-        cout << "\n";
+        this->n = n;
     }
+    void add(int s, int d)
+    {
+        adjlist[s].push_back(d);
+        //adjlist[d].push_back(s);
+    }
+    void dfs_printallpath(int source, int dest)
+    {
+        v[source] = true;
+        path.push_back(source);
+        if (source == dest)
+        {
+            pathexists |= true;
+            allpaths.push_back(path);
+        }
+        for (auto child : adjlist[source])
+        {
+            if (v[child] == 0)
+                dfs_printallpath(child, dest);
+        }
+        path.pop_back();
+        v[source] = false;
+    }
+    void artiutil(int vertex, int parent = -1)
+    {
+        v[vertex] = 1;
+        time = time + 1;
+        d[vertex] = time;
+        low[vertex] = d[vertex];
+        for (auto it = adjlist[vertex].begin(); it != adjlist[vertex].end(); it++)
+        {
+            int child = *it;
+            if (child == parent)
+                continue;
+            if (v[child] == 0)
+            {
+                artiutil(child, vertex);
+                low[vertex] = min(low[vertex], low[child]);
+                if (low[child] > d[vertex])
+                {
+                    cout << vertex << " " << child << endl;
+                }
+            }
+            // u->w
+            else
+            {
+                low[vertex] = min(low[vertex], d[child]);
+            }
+        }
+        v[vertex] = 2;
+        time = time + 1;
+        f[vertex] = time;
+    }
+    void artipoint()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            v[i] = 0;
+            p[i] = NULL;
+            low[i] = INT_MIN;
+            f[i] = INT_MIN;
+            d[i] = INT_MIN;
+        }
+        time = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (v[j] == 0)
+                artiutil(j);
+        }
+    }
+    void printallpath(int s, int d)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            v[i] = 0;
+        }
+        dfs_printallpath(s, d);
+        if (pathexists)
+        {
+            for (auto j : allpaths)
+            {
+                for (auto i : j)
+                {
+                    cout << i << " ";
+                }
+                cout<<endl;
+            }
+        }
+    }
+    Graph getTranspose()
+    {
+        Graph g(n);
+        for (int i = 0; i < n; i++){
+            for(auto j = adjlist[i].begin(); j!=adjlist[i].end();++i){
+                g.add(*j,i);
+            }
+        }
+        return g;
+    }
+    void fillorder(int vertex, bool vis[], stack<int>&st)
+    {
+        vis[vertex] = 1;
+        for (auto child : adjlist[vertex])
+        {
+            if (vis[child] == 0)
+                fillorder(child,vis,st);
+        }
+        st.push(vertex);
+    }
+    void dfsutilscc(int u)
+    {
+        v[u] = 1;
+        cout << u << " ";
+        for (auto w : adjlist[u])
+        {
+            if (v[w] == 0)
+                dfsutilscc(w);
+        }
+    }
+    void printscc()
+    {
+        stack<int> st;
+        bool *vis = new bool[n];
+        for (int i = 0; i < n; i++)
+        {
+            vis[i] = 0;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (vis[i] == 0)
+            {
+                fillorder(i, vis, st);
+            }
+        }
+        Graph gt = getTranspose();
+        for (int i = 0; i < n; i++)
+        {
+            v[i] = 0;
+        }
+        // while(!topostack.empty()){
+        //     int w= topostack.top();
+        //     //cout<<w<<endl;
+        //     topostack.pop();
+        //     if(v[w]==0){
+        //         gt.dfsutilscc(w);
+        //         cout<<endl;
+        //     }
+
+        // }
+
     }
 };
 int main()
@@ -145,14 +188,13 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int n_nodes, n_edges, u, v;
-    scanf("%d",&n_nodes);
-    scanf("%d",&n_edges);
+    scanf("%d", &n_nodes);
+    scanf("%d", &n_edges);
     Graph graph1(n_nodes);
     while (n_edges--)
     {
-        scanf("%d %d",&u, &v);
-        graph1.add(u-1, v-1);
+        scanf("%d %d", &u, &v);
+        graph1.add(u, v);
     }
-    graph1.dfs();
-    
+    graph1.printscc();
 }
